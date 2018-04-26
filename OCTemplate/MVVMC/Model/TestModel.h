238d20a3c818478f4553c038cbc31bf5d3677472
Steven <reversescale@icloud.com>
2018-04-26 13:35:57 +0800

    💬:Updating text and literals.

---
+++ OCTemplate/MVVMC/Model/TestModel.h
@@ -0,0 +1,13 @@
+//
+//  TestModel.h
+//  OCTemplate
+//
+//  Created by WhatsXie on 2018/4/26.
+//  Copyright © 2018年 R.S. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestModel : NSObject
+
+@end
---
+++ OCTemplate/MVVMC/Model/TestModel.m
@@ -0,0 +1,13 @@
+//
+//  TestModel.m
+//  OCTemplate
+//
+//  Created by WhatsXie on 2018/4/26.
+//  Copyright © 2018年 R.S. All rights reserved.
+//
+
+#import "TestModel.h"
+
+@implementation TestModel
+
+@end
