8f1957e3f2f64fc13a33af4bff6c658f1ac16c55
Steven <reversescale@icloud.com>
2018-05-03 13:53:19 +0800

    ✨:Introducing add new react call native features.

--- OCTemplate/React/RNCalliOSAction.m
+++ OCTemplate/React/RNCalliOSAction.m
@@ -24,5 +24,32 @@ RCT_EXPORT_METHOD(calliOSActionWithOneParams:(NSString *)name) {
     [SVProgressHUD showSuccessWithStatus:[NSString stringWithFormat:@"参数：%@",name]];
 }
 
+RCT_EXPORT_METHOD(calliOSActionWithSecondParams:(NSString *)params1 params2:(NSString *)params2) {
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeBlack];
+    [SVProgressHUD showSuccessWithStatus:[NSString stringWithFormat:@"参数1：%@\n参数2:%@",params1,params2]];
+}
+
+RCT_EXPORT_METHOD(calliOSActionWithDictionParams:(NSDictionary *)diction) {
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeBlack];
+    [SVProgressHUD showSuccessWithStatus:[NSString stringWithFormat:@"参数：%@",diction]];
+}
+
+RCT_EXPORT_METHOD(calliOSActionWithArrayParams:(NSArray *)array) {
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeBlack];
+    [SVProgressHUD showSuccessWithStatus:[NSString stringWithFormat:@"参数：%@",array]];
+}
+
+RCT_EXPORT_METHOD(calliOSActionWithActionSheet) {
+    UIAlertController *sheetView=[UIAlertController alertControllerWithTitle:@"RN Call iOS" message:@"RN调用iOS方法弹出ActionSheet"preferredStyle:(UIAlertControllerStyleActionSheet)];
+    UIAlertAction *action1=[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+    }];
+    [sheetView addAction:action1];
+    
+    UIAlertAction *action2=[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+    }];
+    [sheetView addAction:action2];
+    
+    [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:sheetView animated:YES completion:nil];
+}
 
 @end
--- react/index.ios.js
+++ react/index.ios.js
@@ -15,13 +15,48 @@ class Main extends Component {
    render() {
      return (
        <View style={styles.container}>
-         <Text style={styles.welcome}>Welcome to React Native</Text>
-
-         <TouchableOpacity style={{height:30}}
+         <Text style={styles.welcome}>React 和 Native 通信</Text>
+         <TouchableOpacity style={styles.calltonative}
                             onPress={()=>{
                 RNCalliOSAction.calliOSActionWithOneParams('hello');
             }}>
-              <Text>点击调用iOS原生方法,RN向iOS传递一个参数</Text>
+              <Text>点击调用 Native 方法, 并传递一个参数</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.calltonative}
+                            onPress={()=>{
+                RNCalliOSAction.calliOSActionWithSecondParams('hello','iOS');
+            }}>
+              <Text>点击调用 Native 方法, 并传递两个参数</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.calltonative}
+                            onPress={()=>{
+                RNCalliOSAction.calliOSActionWithDictionParams({
+                    params1:'RN',
+                    params2:'call',
+                    params3:'iOS'
+                });
+            }}>
+              <Text>点击调用 Native 方法, 并传递一个 json 数据</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{height:30}}
+                            onPress={()=>{
+                RNCalliOSAction.calliOSActionWithArrayParams([
+                    'RN',
+                    'call',
+                    'iOS'
+                ]);
+            }}>
+              <Text>点击调用 Native 方法, 并传递一个数组</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{height:30}}
+                            onPress={()=>{
+                RNCalliOSAction.calliOSActionWithActionSheet();
+            }}>
+              <Text>点击调用 Native 方法, 弹出 ActionSheet</Text>
           </TouchableOpacity>
 
        </View>
@@ -33,12 +68,17 @@ class Main extends Component {
    container: {
      flex: 1,
      justifyContent: 'center',
+     top: -50,
      alignItems: 'center',
      backgroundColor: '#87e6e3',
    },
    welcome: {
      fontSize: 20,
      color: "#FFFFFF",
+     height:45,
+   },
+   calltonative: {
+     height:30,
    },
  });
  
